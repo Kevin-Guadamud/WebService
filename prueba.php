e65117870a86b8bbf30d789f665609a94cc9469c
Kevin-Guadamud <55851861+Kevin-Guadamud@users.noreply.github.com>
2022-10-25 21:22:55 -0500

    Add files via upload

---
+++ prueba.php
@@ -0,0 +1,13 @@
+<html>
+ <head>
+  <title>UdlaPage Bienvenido</title>
+</head>
+<body>
+<p> Bienvenidos a la pagina web de Kevin.</p>
+ <?php 
+   echo '<p>Esto es una página de prueba</p>'; 
+   echo '<p>Donde podemos verificar</p>';
+   echo '<p>La creación de una página web</p>';
+?>
+</body>
+</html>
